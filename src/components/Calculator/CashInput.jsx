7cd842927e3f6b617cf234c8a28f71ba1d2aa41b
EysteinKS <39171841+EysteinKS@users.noreply.github.com>
2018-06-06 15:50:50 +0200

    Added CashInput

---
+++ src/components/Calculator/CashInput.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class CashInput extends Component {
+  constructor(props){
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p>Cash Input</p>
+      </div>
+
+      /* TODO: ADD FORM
+      https://reactjs.org/docs/forms.html
+       */
+    );
+  }
+}
+
+export default CashInput;
