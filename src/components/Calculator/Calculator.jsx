af5550228f8b0b333c4e2e321ae99fb82bd7e4ea
EysteinKS <39171841+EysteinKS@users.noreply.github.com>
2018-06-07 12:14:32 +0200

    CashInput constructor
    Added "Currency" constructor for CashInput

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,15 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import CashInput from "./CashInput";
 
 class Calculator extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Calculator</h1>
-                <CashInput />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h1>Calculator</h1>
+        <CashInput currency="1000" />
+        <CashInput currency="500" />
+        <CashInput currency="200" />
+        <CashInput currency="100" />
+        <CashInput currency="50" />
+        <CashInput currency="20" />
+        <CashInput currency="10" />
+        <CashInput currency="5" />
+        <CashInput currency="1" />
+      </div>
+    );
+  }
 }
 
 export default Calculator;
--- src/components/Calculator/CashInput.jsx
+++ src/components/Calculator/CashInput.jsx
@@ -1,19 +1,44 @@
 import React, { Component } from "react";
 
 class CashInput extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.state = { value: "" };
+    this.currency = "";
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A number was submitted: " + this.state.value);
+    event.preventDefault();
+  }
 
   render() {
     return (
-      <div>
-        <p>Cash Input</p>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <span>{this.props.currency}: </span>
+          <input
+            type="tel"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Input Money Here"
+          />
+        </label>
+        <span>={this.state.value * this.props.currency}</span>
+      </form>
 
       /* TODO: ADD FORM
       https://reactjs.org/docs/forms.html
+
+      Props: Currency - Output 
+
        */
     );
   }
