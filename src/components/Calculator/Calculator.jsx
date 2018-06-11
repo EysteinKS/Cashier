8b262a6334dd195d119c5d18e4ba21a50064afae
EysteinKS <39171841+EysteinKS@users.noreply.github.com>
2018-06-11 21:58:59 +0200

    Focus & Blur
    Removed reactstrap collapse and used react-collapse instead. Added focus and blur on collapse button.

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,30 +1,80 @@
 import React, { Component } from "react";
+import { Button, Collapse, UncontrolledCollapse } from "reactstrap";
+import "./Calculator.css";
 import CashInput from "./CashInput";
 
 class Calculator extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      collapseCashCounter: false
+    }
+
+    this.toggleCC = this.toggleCC.bind(this);
+  };
+
+  toggleCC() {
+    this.setState({ collapseCashCounter: !this.state.collapseCashCounter });
+  }
+
+
   render() {
     return (
       <div>
         <h1>Calculator</h1>
         <br/>
-        <h3>Total Income</h3>
+        <Button
+          className="toggleButton"
+          color="grey"
+          id="totalincome"
+          style={{ marginBottom: '1rem' }}>
+          <h3>Total Income</h3>
+        </Button>
+        <UncontrolledCollapse toggler="#totalincome">
+          <p>Insert Content Here!</p>
+        </UncontrolledCollapse>
+        <br />
+        <Button
+          className="toggleButton"
+          color="grey"
+          id="registerstart"
+          style={{ marginBottom: '1rem' }}>
+          <h3>Register at Start</h3>
+        </Button>        
+        <UncontrolledCollapse toggler="#registerstart">
+          <p>Insert Content Here!</p>
+        </UncontrolledCollapse>
+        <br />
+        <Button
+          className="toggleButton"
+          color="grey"
+          onClick={this.toggleCC}
+          style={{ marginBottom: '1rem' }}>
+          <h3>Cash Counter</h3>
+        </Button>
+        <Collapse isOpen={this.state.collapseCashCounter}>
+          <CashInput currency="1000" />
+          <CashInput currency="500" />
+          <CashInput currency="200" />
+          <CashInput currency="100" />
+          <CashInput currency="50" />
+          <CashInput currency="20" />
+          <CashInput currency="10" />
+          <CashInput currency="5" />
+          <CashInput currency="1" />
+        </Collapse>
         <br/>
-        <h3>Register at Start</h3>
+        <Button
+          className="toggleButton"
+          color="grey"
+          id="terminalcounter"
+          style={{ marginBottom: '1rem' }}>
+          <h3>Terminal Counter</h3>
+        </Button>
+        <UncontrolledCollapse toggler="#terminalcounter">
+          <p>Insert Content Here!</p>
+        </UncontrolledCollapse>
         <br/>
-        <h3>Register Counter</h3>
-        <CashInput currency="1000" />
-        <CashInput currency="500" />
-        <CashInput currency="200" />
-        <CashInput currency="100" />
-        <CashInput currency="50" />
-        <CashInput currency="20" />
-        <CashInput currency="10" />
-        <CashInput currency="5" />
-        <CashInput currency="1" />
-        <br/>
-        <h3>Terminal Counter</h3>
-        <br/>
-        <h3></h3>
       </div>
     );
   }
--- src/components/Calculator/CashInput.jsx
+++ src/components/Calculator/CashInput.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Button } from "reactstrap";
+import { Collapse } from "react-collapse";
 import "./CashInput.css";
 
 class CashInput extends Component {
@@ -6,10 +8,35 @@ class CashInput extends Component {
     super(props);
     this.state = {
       value: "",
-      result: "0"
+      result: "0",
+      collapse: false,
+      focus: true
     };
+
+    this.toggleCollapse = this.toggleCollapse.bind(this);
+    this.toggleFocus = this.toggleFocus.bind(this);
     this.currency = "";
     this.handleChange = this.handleChange.bind(this);
+    this.textInput = React.createRef();
+    this.focusClick = this.focusClick.bind(this);
+  }
+
+  toggleCollapse() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  toggleFocus(){
+    this.setState({ focus: !this.state.focus});
+  }
+
+  focusClick(){
+    this.toggleCollapse();
+    this.toggleFocus();
+    if (this.state.focus === true ){
+      this.textInput.current.focus();
+    } else {
+      this.textInput.current.blur();
+    }
   }
 
   handleChange(event) {
@@ -28,22 +55,35 @@ class CashInput extends Component {
 
   render() {
     return (
-      <form onKeyPress={this.onKeyPress}>
-        <label className="grid-container">
-          <span className="grid-item">{this.props.currency}: </span>
-          <input
-            className="grid-item"
-            type="tel"
-            value={this.state.value}
-            onChange={this.handleChange}
-            maxLength = "3"
-            placeholder="Input Money Here"
-          />
-          <span className="grid-item">{this.state.result}</span>
-        </label>
-      </form>
+      <div>
+        <Button
+          className = "toggleCashInput"
+          color="grey" 
+          onClick={this.focusClick} 
+          style={{ marginBottom: '1rem'}}>
+          {this.props.currency} = {this.state.result}
+        </Button>
+        <Collapse isOpened={this.state.collapse}>
+          <form onKeyPress={this.onKeyPress}>
+            <label className="grid-container">
+              <input
+                autoFocus
+                className="grid-item-a"
+                type="tel"
+                value={this.state.value}
+                onChange={this.handleChange}
+                maxLength = "3"
+                placeholder="Input Money Here"
+                ref={this.textInput}
+              />
+            </label>
+          </form>
+        </Collapse>
+      </div>
     );
   }
 }
 
 export default CashInput;
+
+//https://reactjs.org/docs/refs-and-the-dom.html
