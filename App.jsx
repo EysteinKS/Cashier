1d37050061f93f415f937bb0905550363f3f8df2
EysteinKS <39171841+EysteinKS@users.noreply.github.com>
2018-10-08 23:32:24 +0200

    Commit via web
    Auth error on Github Desktop :(

---
+++ App.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "./App.css";
+import "./Display.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import * as routes from '../constants/routes';
+import { auth } from "./Firebase"
+
+//App components
+import Header from "./Header";
+import Footer from "./Footer";
+import Home from "./Home.jsx";
+import Calculator from "./Calculator/Calculator";
+import Result from "./Result/Result";
+import Landing from './Server/Landing';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import PasswordForget from './PasswordForget';
+import Account from './Account';
+import withAuthentication from "./withAuthentication";
+
+class App extends Component {
+
+  render(){
+    return(
+      <BrowserRouter>
+        <div className="App" style={{ backgroundColor: "#CAFAFE" }}>
+          <Header className="center" />
+          <Route className="full-height" path="/" exact component={Home} />
+          <Route className="full-height" path="/calculator" component={Calculator} />
+          <Route className="full-height" path="/result" component={Result} />
+          <Route className="full-height" exact path={routes.LANDING} component={() => <Landing />} />
+          <Route className="full-height" exact path={routes.SIGN_UP} component={() => <SignUp />} />
+          <Route className="full-height" exact path={routes.SIGN_IN} component={() => <SignIn />} />
+          <Route className="full-height" exact path={routes.PASSWORD_FORGET} component={() => <PasswordForget />} />
+          <Route className="full-height" exact path={routes.ACCOUNT} component={() => <Account />} />
+          <Footer />
+        </div>
+      </BrowserRouter>
+    )
+  }  
+}
+
+
+
+/*
+Todo:
+Home
+Result
+*/
+  
+/*
+ Colors: http://blog.visme.co/website-color-schemes/ #5
+ Check https://medium.freecodecamp.org/where-do-i-belong-a-guide-to-saving-react-component-data-in-state-store-static-and-this-c49b335e2a00 */
+
+export default withAuthentication(App);
